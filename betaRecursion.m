6a69c4588c72df659266b3b819f64dcc0db852f1
mjedor <matthieu.jedor@gmail.com>
2016-12-23 14:46:34 +0100

    Update betaRecursion.m

--- betaRecursion.m
+++ betaRecursion.m
@@ -1,32 +1,12 @@
-function logBeta = betaRecursion(Y,Pi,P,W,C)
+function logBeta = betaRecursion(Y,Pi,Ptrans,gauss)
     
-    [D,T] = size(Y);
+    T = size(Y,2);
     [M,K] = size(Pi);
     logBeta = zeros(T,K^M);
-    mu = zeros(K^M,D);
-    P_y = zeros(K^M,1);
-    Ptrans=ones(K^M,K^M);
-    
-    % Compute mu, Ptrans
-    states = get_all_states(M,K);
-    for i=1:K^M
-        for m=1:M
-            mu(i,:) = mu(i,:) + W(:,(m-1)*K+states(i,m))';
-        end
-        for j=1:K^M
-            for m=1:M
-                Ptrans(i,j)=Ptrans(i,j)*P((m-1)*K+states(i,m),states(j,m));
-            end
-        end
-    end
     
     for t = T:-1:2
-        for i = 1:K^M
-            P_y(i) = mvnpdf(Y(:,t)',mu(i,:),C);
-        end
-        
         b = max(logBeta(t,:));
-        logBeta(t-1,:) = b + log((P_y'.*exp(logBeta(t,:)-b))*Ptrans'); 
+        logBeta(t-1,:) = b + log((gauss(t,:).*exp(logBeta(t,:)-b))*Ptrans'); 
  
     end
     
