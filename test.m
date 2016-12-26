a1c922357cd3d1defdec14f3cc18fd38bbc144d6
alban-pierre <alban.pierre@ens.fr>
2016-12-27 00:07:05 +0100

    Add gibbs in test.m

--- test.m
+++ test.m
@@ -27,15 +27,27 @@ plot(mu(:,1),mu(:,2),'kx','MarkerSize',15,'LineWidth',3);
 title 'Most likely states according to the marginal probability'
 hold off;
 
-% Exact inference
+%Parameters
 maxIter = 100;
 epsilon = 1e-5;
+
+% Exact inference
 [W1,C1,P1,Pi1,LL1] = em_fhmm(Y,K,M,maxIter,epsilon);
 figure(2);
 plot(1:length(LL1),LL1,'Linewidth',2);
 xlabel('Number of iteration','FontSize',18,'FontWeight','Bold');
 ylabel('Log-likelihood','FontSize',18,'FontWeight','Bold');
 title('Log-likelihood as a function of iteration','FontSize',18,'FontWeight','Bold');
+
+% Gibbs sampling inference
+if (false) % Does not work yet
+[W2,C2,P2,Pi2,LL2] = em_gibbs(Y,K,M,maxIter,epsilon);
+figure(2);
+plot(2:length(LL2),LL2(2:end),'Linewidth',2);
+xlabel('Number of iteration','FontSize',18,'FontWeight','Bold');
+ylabel('Log-likelihood','FontSize',18,'FontWeight','Bold');
+title('Log-likelihood as a function of iteration','FontSize',18,'FontWeight','Bold');
+end
 
 % Completely factorized variational inference
 [W3,C3,P3,Pi3,LL3] = em_cfva(Y,K,M,maxIter,epsilon);
