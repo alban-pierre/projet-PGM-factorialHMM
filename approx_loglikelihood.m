f7da1219d67a5f7813950da3d6e8f35a760d991c
mjedor <matthieu.jedor@gmail.com>
2016-12-27 13:05:36 +0100

    Faster version

--- approx_loglikelihood.m
+++ approx_loglikelihood.m
@@ -1,21 +1,21 @@
 % cf formula A.1 
-function ll = approx_loglikelihood(Y,theta,W,C,P,Pi)
+function ll = approx_loglikelihood(Y,theta,W,invC,P,Pi)
     [D,T] = size(Y);
     [M,K] = size(Pi);
     
     ll = 0;
     
     for t = 1:T
-        ll = ll - 0.5 * Y(:,t)'*pinv(C)*Y(:,t);
+        ll = ll - 0.5 * Y(:,t)' * invC * Y(:,t);
         for m = 1:M
-            ll = ll + Y(:,t)' * pinv(C)*W(:,(m-1)*K+1:m*K) * ...
+            ll = ll + Y(:,t)' * invC * W(:,(m-1)*K+1:m*K) * ...
                 theta(t,(m-1)*K+1:m*K)';
             for n = 1:M
                 if m==n
-                    ll = ll - 0.5 * trace(W(:,(m-1)*K+1:m*K)' * pinv(C) *...
+                    ll = ll - 0.5 * trace(W(:,(m-1)*K+1:m*K)' * invC *...
                         W(:,(m-1)*K+1:m*K) * diag(theta(t,(m-1)*K+1:m*K)));
                 else
-                    ll = ll -0.5 * trace(W(:,(m-1)*K+1:m*K)' * pinv(C) *...
+                    ll = ll -0.5 * trace(W(:,(m-1)*K+1:m*K)' * invC *...
                         W(:,(n-1)*K+1:n*K) * theta(t,(n-1)*K+1:n*K)' * ...
                         theta(t,(m-1)*K+1:m*K));
                 end
@@ -34,6 +34,6 @@ function ll = approx_loglikelihood(Y,theta,W,C,P,Pi)
     ll = ll + theta(1,:) * log(temp);
     
     % Normalization term
-    ll = ll + T * log((2*pi)^(-D/2)/sqrt(det(C)));
+    ll = ll + T * log((2*pi)^(-D/2) * sqrt(det(invC)));
     
 end
