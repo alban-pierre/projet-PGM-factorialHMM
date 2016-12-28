9308ab2512f044d33472cbd2542bb3e96bc419c5
mjedor <matthieu.jedor@gmail.com>
2016-12-28 17:33:14 +0100

    Correct loglikelihood

--- em_gibbs.m
+++ em_gibbs.m
@@ -8,6 +8,8 @@ function [W,C,P,Pi,LL] = em_gibbs(Y,K,M,maxIter,epsilon)
     P = rand(M*K,K);
     P = P ./ sum(P,2);
     LL = [];
+    aLL = [];
+    states = get_all_states(M,K);
     
     %initialisation of gibbs_sampling
     s = zeros(M*K, T);
@@ -33,10 +35,25 @@ function [W,C,P,Pi,LL] = em_gibbs(Y,K,M,maxIter,epsilon)
         % \sum_{t=2}^T <S_t^m S_{t-1}^m>
         sum3 = sum(EStSt, 3);
         
-        % Log-likelihood
+        % Approx log-likelihood
         invC = pinv(C);
-        L = approx_loglikelihood(ESt,ESmSn,EStSt,Y,W,invC,P,Pi);
-        LL = [LL, L];
+        aLL = [aLL, approx_loglikelihood_gibbs(ESt,ESmSn,EStSt,Y,W,invC,P,Pi)];
+        
+        % True log-likelihood
+        Ptrans = computePtrans(P,states);
+        mu = computeMu(W,states);
+        gauss = computeGaussian(Y,mu,C);
+        % Compute Pstates (t=1) for logAlpha1
+        Pstates = ones(1,K^M);
+        for i=1:K^M
+            for m=1:M
+                Pstates(i) = Pstates(i) * Pi(m,states(i,m));
+            end
+        end
+        logAlpha1 = log(Pstates) + log(gauss(1,:));
+        logBeta = betaRecursion(Pi,Ptrans,gauss);
+        ab = max(logAlpha1 + logBeta(1,:),[],2);
+        LL = [LL ab + log(sum(exp(logAlpha1 + logBeta(1,:) - ab),2))];
         
         % M step
         Pi = reshape(ESt(:,1)',[K,M])';
@@ -45,7 +62,7 @@ function [W,C,P,Pi,LL] = em_gibbs(Y,K,M,maxIter,epsilon)
         C = (C+C')/2; % Make sure C is symmetric because of small computations errors
         P = sum3 ./ sum(sum3,2);
     
-        if (tau > 1) && (LL(end) - LL(end-1) < epsilon)
+        if (tau > 1) && (aLL(end) - aLL(end-1) < epsilon)
             break;
         end
     end
