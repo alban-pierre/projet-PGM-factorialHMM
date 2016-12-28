af5305596e19c7e1290b34ee0ff0848e201817c5
mjedor <matthieu.jedor@gmail.com>
2016-12-28 16:52:09 +0100

    Clear code

--- generate_fhmm.m
+++ generate_fhmm.m
@@ -7,10 +7,10 @@
 % P = [P^{1}|...|P^{M}]]'                   MK  * K
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+% clear;
+
 % Variable that is 1 if we use matlab, and 0 otherwise
 isMatlab = exist('OCTAVE_VERSION', 'builtin') == 0;
-
-%clear;
 
 % To be able to repeat
 if (isMatlab)
@@ -47,19 +47,10 @@ Y = zeros(D,T);
 
 % Initialization
 S(:,1) = reshape(mnrnd(1,Pi)',[K*M,1]);
-	 %---%mu = 0;
-	 %---%for m = 1:M
-	 %---%    mu = mu + W(:,((m-1)*K+1):m*K) * S((m-1)*K+1:m*K,1);
-	 %---%end
 mu = sum(W(:,S(:,1)>0),2);
 Y(:,1) = mvnrnd(mu,C);
 
 for t = 2:T
-    %---%mu = 0;
-    %---%for m = 1:M
-    %---%    S((m-1)*K+1:m*K,t) = reshape(mnrnd(1,S((m-1)*K+1:m*K,t-1)'*P((m-1)*K+1:m*K,:)),[K,1]);
-    %---%    mu = mu + W(:,((m-1)*K+1):m*K) * S((m-1)*K+1:m*K,t);
-    %---%end
     S(:,t) = reshape(mnrnd(1,P(S(:,t-1)>0,:))', [K*M,1]);
     mu = sum(W(:,S(:,t)>0),2);
     Y(:,t) = mvnrnd(mu,C);
