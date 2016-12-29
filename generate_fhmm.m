35566e95110cecdcdbbddfacecc067927dff585e
mjedor <matthieu.jedor@gmail.com>
2016-12-29 14:04:43 +0100

    Add new Y for test

--- generate_fhmm.m
+++ generate_fhmm.m
@@ -49,11 +49,17 @@ Y = zeros(D,T);
 S(:,1) = reshape(mnrnd(1,Pi)',[K*M,1]);
 mu = sum(W(:,S(:,1)>0),2);
 Y(:,1) = mvnrnd(mu,C);
+Stest(:,1) = reshape(mnrnd(1,Pi)',[K*M,1]);
+muTest = sum(W(:,Stest(:,1)>0),2);
+Ytest(:,1) = mvnrnd(muTest,C);
 
 for t = 2:T
     S(:,t) = reshape(mnrnd(1,P(S(:,t-1)>0,:))', [K*M,1]);
     mu = sum(W(:,S(:,t)>0),2);
     Y(:,t) = mvnrnd(mu,C);
+    Stest(:,t) = reshape(mnrnd(1,P(Stest(:,t-1)>0,:))', [K*M,1]);
+    muTest = sum(W(:,Stest(:,t)>0),2);
+    Ytest(:,t) = mvnrnd(muTest,C);
 end
 
 plot(Y(1,:),Y(2,:),'.');
