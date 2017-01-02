c33245e5cc5f3e985b8e9628a98598bb3b290c90
alban-pierre <alban.pierre@ens.fr>
2017-01-02 02:07:03 +0100

    Fix experiment2

--- experiment2.m
+++ experiment2.m
@@ -29,16 +29,25 @@ M = 3;
 K = 2;
 [Y,Ytest,Pi,P,W,C] = generate_fhmm(T,K,M,D);
 
+alls = eye(K);
+for m=2:M
+    alls = reshape(repmat(alls,1,K)',K*(m-1),K^m)';
+    alls = [alls, repmat(eye(K), size(alls,1)/K,1)];
+end
+
+
 for t=1:repeat
 
     % Init
+    %ki = kmeans(Y, K^M, 0.01);
+    
     W0 = randn(D,M*K);
     P0 = rand(M*K,K);
     P0 = P0 ./ sum(P0,2);
     
     % Exec
     [W1,C1,P1,Pi1,ll1] = em_fhmm(Y,K,M,maxIter,epsilon,W0,P0);
-    [W2,C2,P2,Pi2,ll2] = em_gibbs(Y,K,M,maxIter,epsilon,W0,P0);
+    %[W2,C2,P2,Pi2,ll2] = em_gibbs(Y,K,M,maxIter,epsilon,W0,P0);
     [W3,C3,P3,Pi3,ll3] = em_cfva(Y,K,M,maxIter,epsilon,W0,P0);
     try     % To avoid errors
         [W4,C4,P4,Pi4,ll4] = em_sva(Y,K,M,maxIter,epsilon,W0,P0);
@@ -47,19 +56,9 @@ for t=1:repeat
         continue
     end
     
-    figure(1); hold off;
-    plot(Y(1,:), Y(2,:), '.b');
-    hold on;
-
-    alls = eye(K);
-    for m=2:M
-        alls = reshape(repmat(alls,1,K)',K*(m-1),K^m)';
-        alls = [alls, repmat(eye(K), size(alls,1)/K,1)];
-    end
-
     mu = W*alls';
     mu1 = W1*alls';
-    mu2 = W2*alls';
+    %mu2 = W2*alls';
     mu3 = W3*alls';
     mu4 = W4*alls';
 
@@ -69,7 +68,7 @@ for t=1:repeat
     hold on;
     plot(mu(1,:), mu(2,:), 'bx', 'MarkerSize',15,'LineWidth',3);
     plot(mu1(1,:), mu1(2,:), 'kx', 'MarkerSize',15,'LineWidth',3);
-    plot(mu2(1,:), mu2(2,:), 'gx', 'MarkerSize',15,'LineWidth',3);
+    %plot(mu2(1,:), mu2(2,:), 'gx', 'MarkerSize',15,'LineWidth',3);
     plot(mu3(1,:), mu3(2,:), 'rx', 'MarkerSize',15,'LineWidth',3);
     plot(mu4(1,:), mu4(2,:), 'mx', 'MarkerSize',15,'LineWidth',3);
 
