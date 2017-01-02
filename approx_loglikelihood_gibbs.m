37a1719ab6f9b165e7e5f31b90439ae3106637c9
mjedor <mjedor@users.noreply.github.com>
2017-01-02 22:30:59 +0100

    Update approx_loglikelihood_gibbs.m

--- approx_loglikelihood_gibbs.m
+++ approx_loglikelihood_gibbs.m
@@ -12,15 +12,20 @@ function ll = approx_loglikelihood_gibbs(ESt,ESmSn,EStSt,Y,W,invC,P,Pi)
             ll = ll + Y(:,t)' * invC * W(:,(m-1)*K+1:m*K) * ...
                 ESt((m-1)*K+1:m*K,t);
             for n = 1:M
-                ll = ll -0.5 * trace(W(:,(m-1)*K+1:m*K)' * invC *...
-                    W(:,(n-1)*K+1:n*K) * ESmSn((n-1)*K+1:n*K,(m-1)*K+1:m*K,t));
+                if m==n
+                    ll = ll - 0.5 * trace(W(:,(m-1)*K+1:m*K)' * invC *...
+                        W(:,(m-1)*K+1:m*K) * diag(ESt((m-1)*K+1:m*K,t)));
+                else
+                    ll = ll -0.5 * trace(W(:,(m-1)*K+1:m*K)' * invC *...
+                        W(:,(n-1)*K+1:n*K) * ESmSn((n-1)*K+1:n*K,(m-1)*K+1:m*K,t));
+                end
             end
             if t < T
                 ll = ll + trace(log(P((m-1)*K+1:m*K,:)) * ...
                     EStSt((m-1)*K+1:m*K,:,t));
             end
             % Normalization term
-            %ll = ll - sum(theta(t,(m-1)*K+1:m*K) .* log(theta(t,(m-1)*K+1:m*K)));
+            %ll = ll - ESt((m-1)*K+1:m*K,t)' * log(ESt((m-1)*K+1:m*K,t));
         end
     end
     
